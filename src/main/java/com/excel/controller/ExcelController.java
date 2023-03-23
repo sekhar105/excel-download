42a739c72d2b59deffde6ffe00495a87dc85de7c
sekhar105 <repaka99999>
2023-03-23 22:17:00 +0530

    added excel to db api

--- src/main/java/com/excel/controller/ExcelController.java
+++ src/main/java/com/excel/controller/ExcelController.java
@@ -1,23 +1,22 @@
 package com.excel.controller;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.aspectj.weaver.ast.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -80,6 +79,24 @@ public void generateExcelReport(HttpServletResponse httpServletResponse) throws 
 	
 	studentService.generate(httpServletResponse);
 }
+@PostMapping("/import")
+public ResponseEntity<?> mapReapExcelDatatoDB(@RequestParam("file") MultipartFile reapExcelDataFile) throws IOException {
 
+   List<Student> tempStudentList = new ArrayList<Student>();
+    XSSFWorkbook workbook = new XSSFWorkbook(reapExcelDataFile.getInputStream());
+    XSSFSheet worksheet = workbook.getSheetAt(0);
+
+    for(int i=1;i<worksheet.getPhysicalNumberOfRows() ;i++) {
+        Student tempStudent = new Student();
+
+        XSSFRow row = worksheet.getRow(i);
+
+        tempStudent.setId((int) row.getCell(0).getNumericCellValue());
+        tempStudent.setName(row.getCell(1).getStringCellValue());
+        tempStudent.setCls(row.getCell(2).getStringCellValue());
+            tempStudentList.add(tempStudent);   
+    }
+    return new ResponseEntity<>(tempStudentList,HttpStatus.OK);
+}
 
 }
